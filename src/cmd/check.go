57bf7adfd8a7f3a6fc5b78e612de6435508dffd3
mac641 <marcel.arndt1@icloud.com>
2022-02-17 14:43:09 +0100

    feat: implement command line parsing

--- src/cmd/check.go
+++ src/cmd/check.go
@@ -20,10 +20,37 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-package main
+package cmd
 
-import "github.com/mac641/gotep/src/cmd"
+import (
+	"fmt"
 
-func main() {
-	cmd.Execute()
+	"github.com/spf13/cobra"
+)
+
+// checkCmd represents the check command
+var (
+	checkCmd = &cobra.Command{
+		Use:   "check",
+		Short: "Check syntax of tests files",
+		Long: `Validate the syntax of test files, as well as associated environment definitions, 
+within gotep. For example run: gotep check -c http-client.env.json -f tests.http`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("check called")
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(checkCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// checkCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
