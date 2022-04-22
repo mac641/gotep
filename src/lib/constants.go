6a859fb8e8d3394074372dd7a49516108b3c9c1e
mac641 <marcel.arndt1@icloud.com>
2022-04-22 12:10:36 +0200

    refactor(src): bundle gloabal vars and constants
    in lib/constants.go
    
      - add TODO comments

--- src/lib/constants.go
+++ src/lib/constants.go
@@ -1,5 +1,7 @@
 package lib
 
+import "regexp"
+
 const (
 	// Terminal output colors
 	Reset  = "\033[0m"
@@ -11,4 +13,19 @@ const (
 	Cyan   = "\033[36m"
 	Gray   = "\033[37m"
 	White  = "\033[97m"
+)
+
+var (
+	// Parser regex
+	// NOTE: multi-line mode flag (?m) enables ^ and $ to match line beginnings and endings, rather than string beginnings and endings
+	regSeparator       = regexp.MustCompile(`(?m)^(###[^\r\n]*(\r?\n|\r))`)
+	regComments        = regexp.MustCompile(`(?m)^(//|#)([^\r\n]*(\r?\n|\r))`)
+	regEnv             = regexp.MustCompile(`{{[ \t\f]*[\w\-]+[ \t\f]*}}`)
+	regResponseHandler = regexp.MustCompile(`(?m)^>[ \t\f]+([^\r\n]*(\r?\n|\r)$|{%(.|(\r?\n|\r))+%})(\r?\n|\r)`)
+	regResponseRef     = regexp.MustCompile(`(?m)^<>[ \t\f]+[^\r\n]*$(\r?\n|\r)`)
+	regRequestline     = regexp.MustCompile(`(?m)^((GET|HEAD|POST|PUT|DELETE|CONNECT|PATCH|OPTIONS|TRACE)[ \t\f]+)?(([\d/\[*]|http|https)[^ \t\f]*)([ \t\f]+(HTTP/\d+(\.\d+)?))?\r?\n|\r$`)
+	regEmptyNewline    = regexp.MustCompile(`(?m)^\r?\n|\r$`)
+	regLineEnding      = regexp.MustCompile(`\r?\n|\r`)
+	regHeaders         = regexp.MustCompile(`(?m)^(?P<Fieldname>[\w\-]+):[ \t\f]*(?P<Fieldvalue>[^\r\n]+[ \t\f]*)$`)
+	regInputFileRef    = regexp.MustCompile(`(?m)^<[ \t\f]+(?P<Filepath>[^\r\n]+)$`)
 )
